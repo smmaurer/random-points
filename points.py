6fe5b2cf017018284fca74e2268437548299bd1c
Sam Maurer <maurer@berkeley.edu>
2017-01-17 11:14:34 -0800

    Adding script file

---
+++ points.py
@@ -0,0 +1,21 @@
+
+
+# Load a shapefile for the bay area
+
+
+# Project into meters
+
+
+# Generate random points within min/max bounds
+
+
+# Check if they're within the shapefile bounds
+
+
+# Save id, lat/lon, link to Google
+
+
+# Save as CSV and as GeoJSON
+
+
+# Stop when we have 100
